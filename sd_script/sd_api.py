d3dbcfb2c5fab6c99fb67a797c2e1bc3e5372fe2
Jack Cui <c411184003@gmail.com>
2023-05-21 16:19:32 +0800

    Update sd_api.py

--- sd_script/sd_api.py
+++ sd_script/sd_api.py
@@ -16,14 +16,6 @@ def decode_image(src):
     """
     解码图片
     :param src: 图片编码
-        eg:
-            src="data:image/gif;base64,R0lGODlhMwAxAIAAAAAAAP///
-                yH5BAAAAAAALAAAAAAzADEAAAK8jI+pBr0PowytzotTtbm/DTqQ6C3hGX
-                ElcraA9jIr66ozVpM3nseUvYP1UEHF0FUUHkNJxhLZfEJNvol06tzwrgd
-                LbXsFZYmSMPnHLB+zNJFbq15+SOf50+6rG7lKOjwV1ibGdhHYRVYVJ9Wn
-                k2HWtLdIWMSH9lfyODZoZTb4xdnpxQSEF9oyOWIqp6gaI9pI1Qo7BijbF
-                ZkoaAtEeiiLeKn72xM7vMZofJy8zJys2UxsCT3kO229LH1tXAAAOw=="
-
     :return: str 保存到本地的文件名
     """
     # 1、信息提取
